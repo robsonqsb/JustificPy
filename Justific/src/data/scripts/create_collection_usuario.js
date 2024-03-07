8cf21e773b8ef4a2ba3dc442306ad84b41192865
Robson Oliveira da Silva <robsonqsb@hotmail.com>
2024-03-06 23:22:32 -0300

    continuação das implementações iniciais criação do primeiro repositório inclusão da dependência python .env

--- Justific/src/data/scripts/create_collection_usuario.js
+++ Justific/src/data/scripts/create_collection_usuario.js
@@ -1,30 +1,32 @@
 db.createCollection("usuario", {
     validator : {
-        bsonType: "object",
-        required: ["login", "senha", "data_criacao", "excluido"],
-        properties: {
-            login: {
-                bsonType: "string",
-                minLength: 3,
-                maxLenght: 20,
-                description: "login inválido"
-            },
-            senha: {
-                bsonType: "string",
-                minLength: 5,
-                maxLength: 50,
-                description: "senha inválida"
-            },
-            data_criacao: {
-                bsonType: "timestamp",
-                description: "informe a data de criação do registro"
-            },
-            alterado_em: {
-                bsonType: "timestamp"
-            },
-            excluido: {
-                bsonType: "bool",
-                description: "informe se o registro é excluido"
+        $jsonSchema:{
+            bsonType: "object",
+            required: ["login", "senha", "data_criacao", "excluido"],
+            properties: {
+                login: {
+                    bsonType: "string",
+                    minLength: 3,
+                    maxLenght: 20,
+                    description: "login inválido"
+                },
+                senha: {
+                    bsonType: "string",
+                    minLength: 5,
+                    maxLength: 50,
+                    description: "senha inválida"
+                },
+                data_criacao: {
+                    bsonType: "date",
+                    description: "informe a data de criação do registro"
+                },
+                alterado_em: {
+                    bsonType: ["date", "null"]
+                },
+                excluido: {
+                    bsonType: "bool",
+                    description: "informe se o registro é excluido"
+                }
             }
         }
     }
